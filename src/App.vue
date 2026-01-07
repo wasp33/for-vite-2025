fb8845c257c2b3ea5c37059a68f04d2194277914
Joseph Liu <josephliu@digiwin.com>
2026-01-07 02:13:18 +0000

    check in code

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue';
+
+const num =ref(0);
+function onClick() {
+  num.value += 1;
+}
+
+</script>
+
+<template>
+  <div>
+    <h1>Hello Vite!</h1>
+    <button type="button" v-on:click="onClick"> Click Me </button>
+    <p>Button clicked {{ num }} times.</p>
+  </div>
+  
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
